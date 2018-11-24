eee2bcbe7cbf6cb84fe99ce7fe4fcaa7d422e9cb
godfrey Timba <gntimba@gmail.com>
2018-11-24 12:01:21 +0200

    database

--- application/views/list_album.php
+++ application/views/list_album.php
@@ -6,7 +6,7 @@
 <center>
 	<div>
 	
-		<table class="table table-striped table-dark">
+		<table class="table table-striped table-dark table-fixed">
 			<thead>
 				<tr>
 					<th scope="col">Picture</th>
